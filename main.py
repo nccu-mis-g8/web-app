5b3b88d0a9045268192055387e6a30ec4e8267e0
LinZKai <linsonlineric@gmail.com>
2024-08-14 18:57:00 +0800

    add login page

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def hello():  
     return render_template('index.html')
 
+@app.route("/login")
+def login():
+   return render_template('login.html')
+
 
 if __name__ == "__main__":
   # debugging 
---
+++ static/js/login.js
@@ -0,0 +1,17 @@
+﻿const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const loginButton = document.getElementById("login-button");
+
+function checkInputs() {
+    const usernameValue = usernameInput.value.trim();
+    const passwordValue = passwordInput.value.trim();
+
+    if(usernameValue && passwordValue) {
+        loginButton.disabled = false;
+    } else {
+        loginButton.disabled = true;
+    }
+}
+
+usernameInput.addEventListener("input", checkInputs);
+passwordInput.addEventListener("input", checkInputs);
