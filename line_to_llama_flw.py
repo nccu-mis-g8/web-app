65a75bc861beb247ce8a74b97b6af618bc4abea2
LinZKai <linsonlineric@gmail.com>
2024-09-11 00:10:50 +0800

    fix logout problem

--- line_to_llama_flw.py
+++ line_to_llama_flw.py
@@ -7,87 +7,100 @@ import csv
 # 自己的LINE帳號名稱
 # master = "陳奕利Jefferson Chen"
 # 放LINE聊天記錄txt檔的目錄
+data_dir = "./upload_files"
 
-class LineChatProcessor:
-    def __init__(self, data_dir="./", output_file_name="training_data_flw.csv", master_name="祐辰"):
-        self.data_dir = data_dir
-        self.output_file_name = output_file_name
-        self.master_name = master_name
-        self.instructions_list = []
-        self.inputs_list = []
-        self.outputs_list = []
+# 輸出檔案的檔名
+output_file_name = "training_data_flw.csv"
 
-    def is_master(self, name):
-        return self.master_name in name
-    
-    def create_formatted_content(self, file_name):
-        instruction = ""
-        input = ""
-        output = ""
-        lines = None
-        with open(file_name, encoding="utf-8") as f:
-            lines = f.readlines()
-        if lines is None:
-            return
-        pre_is_master = False
 
-        for i in range(4, len(lines)):
-            if (lines[i]) == "\n":
-                continue
-            if lines[i].endswith("已收回訊息"):
-                continue
-            w = lines[i].split("\t")
-            if len(w) < 3:
-                continue
-            if "收回訊息" in w[2]:
-                continue
-            if self.is_master(w[1]):
-                output += w[2]
-                pre_is_master = True
-            else:
-                if pre_is_master:
-                    self.instructions_list.append(instruction)
-                    self.inputs_list.append(input)
-                    self.outputs_list.append(output)
-                    instruction = ""
-                    input = ""
-                    output = ""
-                instruction += w[2]
-                pre_is_master = False
+instructions_list = []
+inputs_list = []
+outputs_list = []
 
-    def output_file(self):
-        if (
-            len(self.instructions_list) != len(self.inputs_list)
-            or len(self.inputs_list) != len(self.outputs_list)
-            or len(self.instructions_list) != len(self.outputs_list)
-        ):
-            return
-        block_title = "=你是好友，也是大學同學。請以好友的回氣回答對話。"
-        with open(self.output_file_name, "w", encoding="utf-8") as writer:
-            fieldnames = ["instruction", "input", "output", "text"]
-            writer = csv.DictWriter(writer, fieldnames=fieldnames)
-            writer.writeheader()
-            for i in range(len(self.instructions_list)):
-                writer.writerow(
-                    {
-                        "instruction": self.instructions_list[i],
-                        "input": "",
-                        "output": self.outputs_list[i],
-                        "text": block_title,
-                    }
-                )
-        
-    def process(self):
-        files = [
-            f
-            for f in listdir(self.data_dir)
-            if isfile(join(self.data_dir, f)) and f.endswith("的聊天.txt")
-        ]
-        for f in files:
-            self.create_formatted_content(f)
-        self.output_file()
-        print("done...")
-        print(f"total length: {len(self.instructions_list)}")
-        file_size = os.path.getsize(self.output_file_name)
-        print(f"data size: {file_size/1024**2}mb")
+#要記得改
+master_name = "***"
 
+
+def isMaster(name):
+    return master_name in name
+
+
+# 讀每個檔案
+def create_formatted_content(file_name):
+    instruction = ""
+    input = ""
+    output = ""
+    lines = None
+    with open(file_name, encoding="utf-8") as f:
+        lines = f.readlines()
+    if lines == None:
+        return
+    pre_is_master = False
+
+    for i in range(4, len(lines)):
+        # 去掉空白行
+        if (lines[i]) == "\n":
+            continue
+        if lines[i].endswith("已收回訊息"):
+            continue
+        w = lines[i].split("\t")
+        # 去掉日期那行
+        if len(w) < 3:
+            continue
+        if "收回訊息" in w[2]:
+            continue
+        if isMaster(w[1]):
+            output += w[2]
+            pre_is_master = True
+        else:
+            if pre_is_master:
+                instructions_list.append(instruction)
+                inputs_list.append(input)
+                outputs_list.append(output)
+                instruction = ""
+                input = ""
+                output = ""
+            instruction += w[2]
+            pre_is_master = False
+
+
+# 輸出檔案
+def output_file(instructions_list, inputs_list, outputs_list):
+    # 三個list不一樣就不處理
+    if (
+        len(instructions_list) != len(inputs_list)
+        or len(inputs_list) != len(outputs_list)
+        or len(instructions_list) != len(outputs_list)
+    ):
+        return
+    block_title = "=你是好友，也是大學同學。請以好友的回氣回答對話。"
+    with open(output_file_name, "w", encoding="utf-8") as writer:
+        fieldnames = ["instruction", "input", "output", "text"]
+        writer = csv.DictWriter(writer, fieldnames=fieldnames)
+        writer.writeheader()
+        for i in range(len(instructions_list)):
+            writer.writerow(
+                {
+                    "instruction": instructions_list[i],
+                    "input": "",
+                    "output": outputs_list[i],
+                    "text": block_title,
+                }
+            )
+
+
+def handle_txt_file():
+    # 拿到所有是聊天記錄的txt檔
+    files = [
+        f
+        for f in listdir(data_dir)
+        if isfile(join(data_dir, f)) and f.endswith("的聊天.txt")
+    ]
+    #
+    for f in files:
+        create_formatted_content(f)
+    output_file(instructions_list, inputs_list, outputs_list)
+    print("done...")
+    print(f"total length: {len(instructions_list)}")
+    file_size = os.path.getsize(output_file_name)
+    print(f"data size: {file_size/1024**2}mb")
