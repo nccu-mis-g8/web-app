79b43e1af1e42f045b8f73526d24c112ff5d27df
LinZKai <linsonlineric@gmail.com>
2024-09-22 02:11:34 +0800

    adjust for upload function

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,14 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import PrivateRoute from "./routes/PrivateRoute";
 import ChatRoom from "./routes/ChatRoom";
 import Login from "./routes/Login";
+import Upload from "./routes/Upload";
 
 
 
 const router = createBrowserRouter([
     { path: "/", element: <PrivateRoute element={<ChatRoom />} /> },
     { path: "/login", element: <Login /> },
+    { path: "/upload", element: <Upload /> },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- gui.py
+++ gui.py
@@ -2,4 +2,4 @@
 from main import app
 
 if __name__ == "__main__":
-  FlaskUI(app=app, server="flask").run()
+  FlaskUI(app=app, server="flask", port=5002).run()
