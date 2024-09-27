c8f52b939f42a7d40f8f8ec2a0d900d7a8521964
LinZKai <linsonlineric@gmail.com>
2024-09-27 16:57:56 +0800

    adjust register style and API

--- frontend/src/components/register/RegisterBackground.js
+++ frontend/src/components/register/RegisterBackground.js
@@ -1,10 +1,14 @@
-﻿import classes from "./RegisterBackground.module.css";
+﻿import { Link } from "react-router-dom";
+import RegisterMainFrame from "./RegisterMainFrame";
+import classes from "./RegisterBackground.module.css";
 
 function RegisterBackground() {
     return (
         <div className={classes.container}>
             <div className={classes.outerWrap}>
                 <div className={classes.title}>註冊</div>
+                <RegisterMainFrame />
+                <Link to="/login" className={classes.backToLogin}>返回登入</Link>
             </div>
         </div>
     );
---
+++ frontend/src/components/register/RegisterMainFrame.js
@@ -0,0 +1,178 @@
+﻿import { useState, useEffect } from "react";
+import { Form, useNavigate } from "react-router-dom";
+import openEye from "../../images/openEye.png";
+import closeEye from "../../images/closeEye.png";
+import classes from "./RegisterMainFrame.module.css";
+
+function RegisterMainFrame() {
+    const [lastName, setLastName] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [account, setAccount] = useState("");
+    const [password, setPassword] = useState("");
+    const [checkPassword, setCheckPassword] = useState("");
+    const [isButtonEnabled, setIsButtonEnabled] = useState(false);
+    const [accountError, setAccountError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [checkPasswordError, setCheckPasswordError] = useState("");
+    // for password
+    const [showPasswordState, setShowPasswordState] = useState(false);
+    // for check password
+    const [showPasswordState2, setShowPasswordState2] = useState(false);
+
+    const navigate = useNavigate();
+    const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+    useEffect(() => {
+        if (lastName && firstName && account && password && checkPassword) {
+            setIsButtonEnabled(true);
+        } else {
+            setIsButtonEnabled(false);
+        }
+    }, [lastName, firstName, account, password, checkPassword]);
+
+    function accountChangeHandler(e) {
+        setAccount(e.target.value);
+        setAccountError("");
+    }
+    
+    function passwordChangeHandler(e) {
+        setPassword(e.target.value);
+        setPasswordError("");
+        setCheckPasswordError("");
+    }
+
+    function checkPasswordChangeHandler(e) {
+        setCheckPassword(e.target.value);
+        setPasswordError("");
+        setCheckPasswordError("");
+    }
+
+    function togglePasswordVisibility() {
+        setShowPasswordState(!showPasswordState);
+    }
+
+    function togglePasswordVisibility2() {
+        setShowPasswordState2(!showPasswordState2);
+    }
+
+    async function submitHandler(e) {
+        e.preventDefault();
+
+        if (!passwordPattern.test(password)) {
+            setPasswordError("密碼不符合格式");
+            return
+        }
+
+        if (password !== checkPassword) {
+            setCheckPasswordError("密碼不一致");
+            return
+        }
+
+        try {
+            const response = await fetch("http://127.0.0.1:5001/auth/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    account: account,
+                    firstname: firstName,
+                    lastname: lastName,
+                    password: password,
+                }),
+            });
+
+            if (response.status === 200) {
+                console.log("註冊成功");
+                alert("註冊成功");
+                navigate("/login"); // 重新導向到登入頁面
+            } else if (response.status === 400) {
+                const responseData = await response.json();
+                const error = responseData.message;
+                setAccountError(error);
+                console.log("錯誤訊息" + error);
+            } else {
+                const responseData = await response.json();
+                const error = responseData.message;
+                console.error(error);
+                alert("發生錯誤，請重新整理後再試一次");
+            }
+        } catch (error) {
+            console.error("Error durning login: ", error);
+        }
+    }
+
+    return (
+        <Form className={classes.formContainer} onSubmit={submitHandler}>
+            <div className={classes.label}>
+                <span>姓名</span>
+                <span className={classes.starSign}> *</span>
+            </div>
+            <div className={classes.nameGroup}>
+                <div className={classes.lastNameInput}>
+                    <input
+                        placeholder="姓氏"
+                        type="text"
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+                </div>
+                <div className={classes.firstNameInput}>
+                    <input
+                        placeholder="名字"
+                        type="text"
+                        onChange={(e) => setFirstName(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div className={classes.label}>
+                <span>帳號</span>
+                <span className={classes.starSign}> *</span>
+                <span className={classes.errorMessage}>{accountError}</span>
+            </div>
+            <div className={classes.inputGroup}>
+                <input
+                    placeholder="帳號"
+                    type="text"
+                    onChange={accountChangeHandler}
+                />
+            </div>
+            <div className={classes.label}>
+                <span>密碼</span>
+                <span className={classes.starSign}> *</span>
+                <span className={classes.errorMessage}>{passwordError}</span>
+            </div>
+            <div className={classes.inputGroup2}>
+                <input
+                    placeholder="請輸入8位以上由字母和數字組成的密碼"
+                    type={showPasswordState ? "text" : "password"}
+                    onChange={passwordChangeHandler}
+                />
+                <img
+                    src={showPasswordState ? openEye : closeEye}
+                    className={classes.eyeIcon}
+                    onClick={togglePasswordVisibility}
+                    alt="隱藏密碼"
+                />
+            </div>
+            <div className={classes.errorMessage2}>{checkPasswordError}</div>
+            <div className={classes.inputGroup}>
+                <input
+                    placeholder="請再次輸入密碼"
+                    type={showPasswordState2 ? "text" : "password"}
+                    onChange={checkPasswordChangeHandler}
+                />
+                <img
+                    src={showPasswordState2 ? openEye : closeEye}
+                    className={classes.eyeIcon}
+                    onClick={togglePasswordVisibility2}
+                    alt="隱藏密碼"
+                />
+            </div>
+            <button className={classes.loginButton} disabled={!isButtonEnabled}>
+                送出
+            </button>
+        </Form>
+    );
+}
+
+export default RegisterMainFrame;
