b8b543aba847b0c9a548c3af411e8dcae6a0e972
LinZKai <linsonlineric@gmail.com>
2024-10-04 23:03:24 +0800

    change register to new style

--- frontend/src/components/register/RegisterBackground.js
+++ frontend/src/components/register/RegisterBackground.js
@@ -1,14 +1,16 @@
 ﻿import { Link } from "react-router-dom";
 import RegisterMainFrame from "./RegisterMainFrame";
 import classes from "./RegisterBackground.module.css";
+import logo from "../../images/logo.png";
 
 function RegisterBackground() {
     return (
         <div className={classes.container}>
             <div className={classes.outerWrap}>
-                <div className={classes.title}>註冊</div>
+                <div className={classes.title}>Sign up</div>
                 <RegisterMainFrame />
                 <Link to="/login" className={classes.backToLogin}>返回登入</Link>
+                <img src={logo} alt="langemotion" className={classes.logo} />
             </div>
         </div>
     );
