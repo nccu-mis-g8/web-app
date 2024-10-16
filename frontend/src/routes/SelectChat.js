e158c54c0a2a8a0c4c55027b57aa89cf99fb179a
LinZKai <linsonlineric@gmail.com>
2024-10-16 20:59:25 +0800

    add select chat page route

---
+++ frontend/src/routes/SelectChat.js
@@ -0,0 +1,18 @@
+﻿import NavigationBar from "../components/NavigationBar";
+import SelectChatMainFrame from "../components/chat/SelectChatMainFrame";
+import classes from "./SelectChat.module.css";
+
+function SelectChat() {
+    return (
+        <>
+            <div className={classes.container}>
+                <div className={classes.barContainer}>
+                    <NavigationBar />
+                </div>
+                <SelectChatMainFrame />
+            </div>
+        </>
+    );
+}
+
+export default SelectChat;
