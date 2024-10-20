bbb621a9dc95c00c347a53bc0b9d9b358d84f946
LinZKai <linsonlineric@gmail.com>
2024-10-21 01:26:04 +0800

    Add transition animation for successful login

--- frontend/src/components/login/LoginBackground.js
+++ frontend/src/components/login/LoginBackground.js
@@ -1,14 +1,30 @@
-﻿import classes from "./LoginBackground.module.css";
-import { Link } from "react-router-dom";
+﻿import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import classes from "./LoginBackground.module.css";
 import LoginMainFrame from "./LoginMainFrame";
 import logo from "../../images/logo.png";
 
 function LoginBackground() {
+    const [isExpanding, setIsExpanding] = useState(false);
+
+    const navigate = useNavigate();
+
+    function loginSuccessHandler() {
+        // 當登入成功後，觸發動畫
+        setIsExpanding(true);
+
+        // 設置延遲，與動畫時間匹配，然後進行頁面跳轉
+        setTimeout(() => {
+            navigate("/");
+            console.log("動畫結束，跳轉到主頁");
+        }, 800); // 與動畫時間一致
+    }
+
     return (
         <div className={classes.container}>
-            <div className={classes.outerWrap}>
+            <div className={`${classes.outerWrap} ${isExpanding ? classes.expand : ""}`}>
                 <div className={classes.title}>Login</div>
-                <LoginMainFrame />
+                <LoginMainFrame onLoginSuccess={loginSuccessHandler} />
                 <div className={classes.loginBottom}>
                     <Link to="/register" className={classes.register}>
                         註冊帳號
