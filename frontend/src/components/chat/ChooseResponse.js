a924fb41d685b2a8f412e08711b7a8ce67e2288f
LinZKai <linsonlineric@gmail.com>
2024-11-24 02:21:12 +0800

    Update selected content and its styling

--- frontend/src/components/chat/ChooseResponse.js
+++ frontend/src/components/chat/ChooseResponse.js
@@ -1,35 +1,35 @@
 ﻿import classes from "./ChooseResponse.module.css";
 import def_avatar from "../../images/default_avatar.png";
 
-function ChooseResponse() {
+function ChooseResponse({ res1, res2, onChoose, avatar }) {
     return (
         <div>
             <div className={classes.title}>請選擇更符合期待的回答</div>
             <div className={classes.responseContainer}>
-                <div className={classes.response}>
+                <div className={classes.response} onClick={() => onChoose(res1)}>
                     <div className={classes.responseHeader}>
                         <img
-                            src={def_avatar}
+                            src={avatar}
                             alt="頭像"
                             className={classes.avatar}
                         />
                         <div className={classes.responseTitle}>回應1</div>
                     </div>
                     <div className={classes.content}>
-                        聽起來酷爆ㄌ！ 這個機器人實際上會怎麼陪伴阿？超好奇耶
+                        {res1}
                     </div>
                 </div>
-                <div className={classes.response}>
+                <div className={classes.response} onClick={() => onChoose(res2)}>
                     <div className={classes.responseHeader}>
                         <img
-                            src={def_avatar}
+                            src={avatar}
                             alt="頭像"
                             className={classes.avatar}
                         />
                         <div className={classes.responseTitle}>回應2</div>
                     </div>
                     <div className={classes.content}>
-                        哇哇炫炮的 idea ~ 那實際功能是甚麼勒
+                        {res2}
                     </div>
                 </div>
             </div>
