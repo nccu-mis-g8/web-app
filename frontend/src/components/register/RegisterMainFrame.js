94aaa4cf4a9d32502599ae4fd6d017fc4a399f0f
LinZKai <linsonlineric@gmail.com>
2024-11-24 02:41:57 +0800

    change url api

--- frontend/src/components/register/RegisterMainFrame.js
+++ frontend/src/components/register/RegisterMainFrame.js
@@ -79,7 +79,7 @@ function RegisterMainFrame() {
         }
 
         try {
-            const response = await fetch("http://127.0.0.1:5001/auth/register", {
+            const response = await fetch("https://nccu-group-8.work/auth/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
