d70a5785df8bccc432bc2350bb446d80c9f17c72
LinZKai <linsonlineric@gmail.com>
2024-11-24 02:11:13 +0800

    Add main routes for event-related pages

---
+++ frontend/src/routes/Event.js
@@ -0,0 +1,18 @@
+﻿import NavigationBar from "../components/NavigationBar";
+import CheckEvent from "../components/notepad/CheckEvent";
+import classes from "./ChatRoom.module.css";
+
+function Event() {
+    return (
+        <>
+            <div className={classes.container}>
+                <div className={classes.barContainer}>
+                    <NavigationBar />
+                </div>
+                <CheckEvent />
+            </div>
+        </>
+    )
+}
+
+export default Event;
---
+++ frontend/src/routes/EventContent.js
@@ -0,0 +1,20 @@
+﻿import { useParams } from "react-router-dom";
+import NavigationBar from "../components/NavigationBar";
+import EventContentMainFrame from "../components/notepad/EventContentMainFrame";
+import classes from "./EventContent.module.css";
+
+function EventContent() {
+
+    return (
+        <>
+            <div className={classes.container}>
+                <div className={classes.barContainer}>
+                    <NavigationBar />
+                </div>
+                <EventContentMainFrame />
+            </div>
+        </>
+    );
+}
+
+export default EventContent;
