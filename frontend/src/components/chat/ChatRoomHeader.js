76d0c9aca5e69dc33c03b527832daecb1936d48a
LinZKai <linsonlineric@gmail.com>
2024-11-24 02:24:24 +0800

    Add animation for copying and sharing link

--- frontend/src/components/chat/ChatRoomHeader.js
+++ frontend/src/components/chat/ChatRoomHeader.js
@@ -1,14 +1,23 @@
-﻿import { useNavigate } from "react-router-dom";
+﻿import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import classes from "./ChatRoomHeader.module.css";
 import goBack from "../../images/goBack.png";
 import share from "../../images/share.png";
 import question from "../../images/question.png";
 
 function ChatRightHeader({name}) {
+    const [showTooltip, setShowTooltip] = useState(false);
     const navigate = useNavigate();
 
     function goBackHandler() {
         navigate("/");
+    }
+
+    function shareHandler() {
+        setShowTooltip(true);
+        setTimeout(() => {
+            setShowTooltip(false); // 1.5 秒後隱藏提示
+        }, 1500);
     }
 
     return (
@@ -19,10 +28,13 @@ function ChatRightHeader({name}) {
                     <span className={classes.name}>{name}</span>
                 </div>
                 <div className={classes.right}>
-                    <img src={share} alt="分享" className={classes.share} />
+                    <img src={share} alt="分享" className={classes.share} onClick={shareHandler} />
                     <img src={question} alt="更多資訊" className={classes.question} />
                 </div>
             </div>
+            {showTooltip && (
+                <div className={classes.tooltip}>已複製分享連結</div>
+            )}
         </div>
     );
 }
